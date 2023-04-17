91077e277cce5bf09518dd5b143a1ff376986ef8
Jhean Carlos Pineros Diaz <jhean.pineros@mercadolibre.com.co>
2023-04-17 08:58:18 -0500

    Initial project

---
+++ OnePercent/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  OnePercent
+//
+//  Created by Jhean Carlos Pineros Diaz on 13/04/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        super.tableView(tableView, cellForRowAt: indexPath)
+    }
+
+
+}
+
