4c562e4bcaccac35745b1ffc3cb6a4a065f42df8
Jhean Carlos Pineros Diaz <jhean.pineros@mercadolibre.com.co>
2023-04-28 08:42:36 -0500

    Set default navigation view controller through the Scene delegate

---
+++ OnePercent/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  OnePercent
+//
+//  Created by Jhean Carlos Pineros Diaz on 28/04/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
